d8b9b341b108f93062eeb606eadf97adf2910256
Arun Elanthamil <arun.elanthamil@superops.ai>
2025-05-31 12:51:40 +0530

    [Feat] Project edit and update.

--- lib/download.ts
+++ lib/download.ts
@@ -1,70 +1,48 @@
-import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
-import JSZip from "npm:jszip";
-import { writeFile } from "node:fs/promises";
-
+import { createClient } from "@supabase/supabase-js";
+import JSZip from "jszip";
+import { writeFile } from "fs/promises";
 
 // Update the url and key from env
-const supabase = createClient(
-    "",
-    "",
-);
+const supabase = createClient("", "");
 
 export async function download(projectId: string, languages: string[]) {
+  const translationResponse = await supabase
+    .from("translatedKeys")
+    .select("keys(id, name), translations")
+    .eq("project_id", projectId);
 
-    const translationResponse = await supabase.from("translatedKeys").select('keys(id, name), translations').eq("project_id", projectId);
+  if (!translationResponse.data) {
+    console.log("Invalid Project");
+  }
 
-    if (!translationResponse.data) {
-        console.log("Invalid Project")
-    }
+  const translations: { [key: string]: { [key: string]: string } } = {};
 
-    const translations: { [key: string]: { [key: string]: string } } = {}
+  // const data: { [key: string]: string } = {}
 
-    // const data: { [key: string]: string } = {}
-
-    translationResponse.data?.forEach((e) => {
-        languages.forEach((langCode) => {
-            if (translations[`${langCode}`] == null) {
-                translations[`${langCode}`] = {};
-            }
-            translations[`${langCode}`][`${e.keys.name}`] = `${e.translations[langCode]}`;
-        });
-    })
-
-
-    console.log(translations);
-
-
-    // Zip the temp directory
-    const zip = new JSZip();
-
+  translationResponse.data?.forEach((e: any) => {
     languages.forEach((langCode) => {
-        const data = translations[langCode];
-        const fileName = `${langCode}.json`;
-        zip.file(fileName, JSON.stringify(data, null, 2));
+      if (translations[`${langCode}`] == null) {
+        translations[`${langCode}`] = {};
+      }
+      translations[`${langCode}`][`${e.keys?.name}`] =
+        `${e.translations[langCode]}`;
     });
+  });
 
-    // Generate ZIP buffer and write to file
-    const content = await zip.generateAsync({ type: "nodebuffer" });
-    await writeFile("output.zip", content);
+  console.log(translations);
 
-    console.log("✅ output.zip created.");
+  // Zip the temp directory
+  const zip = new JSZip();
 
+  languages.forEach((langCode) => {
+    const data = translations[langCode];
+    const fileName = `${langCode}.json`;
+    zip.file(fileName, JSON.stringify(data, null, 2));
+  });
+
+  // Generate ZIP buffer and write to file
+  const content = await zip.generateAsync({ type: "nodebuffer" });
+  await writeFile("output.zip", content);
+
+  console.log("✅ output.zip created.");
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
