7e9ef2466a653f9fd8e9daeb523f173b70af67b6
Arun Elanthamil <arun.elanthamil@superops.ai>
2025-06-23 03:34:11 +0530

    [Feat] Moved app logic to different repo.

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -18,26 +18,26 @@ const Footer = () => {
     getUser();
   }, []);
 
-  if (user) {
-    return (
-      <>
-        <div className="h-px w-full bg-gradient-to-r from-transparent via-[#D2D8E183] to-transparent dark:via-[#959CB183]"></div>
-        <div className="py-8">
-          <p className="text-center text-base text-body-color dark:text-white">
-            LanQ Inc.{" "}
-            <a
-              href="https://supabase.com/?utm_source=create-next-app&utm_medium=template&utm_term=nextjs"
-              target="_blank"
-              className="font-bold hover:underline"
-              rel="noreferrer"
-            >
-              ©️
-            </a>
-          </p>
-        </div>
-      </>
-    );
-  }
+  // if (user) {
+  return (
+    <>
+      <div className="h-px w-full bg-gradient-to-r from-transparent via-[#D2D8E183] to-transparent dark:via-[#959CB183]"></div>
+      <div className="py-8">
+        <p className="text-center text-base text-body-color dark:text-white">
+          LanQ Inc.{" "}
+          <a
+            href="https://supabase.com/?utm_source=create-next-app&utm_medium=template&utm_term=nextjs"
+            target="_blank"
+            className="font-bold hover:underline"
+            rel="noreferrer"
+          >
+            ©️
+          </a>
+        </p>
+      </div>
+    </>
+  );
+  // }
   return (
     <footer className="w-full relative z-10 bg-white pt-8 dark:bg-gray-dark md:pt-20 lg:pt-12">
       <div className="container">
