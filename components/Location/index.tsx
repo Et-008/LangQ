2408b6f94da531d3e61900c15d7cc32d9cfbcb0d
Arun Elanthamil <arun.elanthamil@superops.ai>
2025-08-09 14:31:31 +0530

    [Feat] Location detection added.

--- components/Location/index.tsx
+++ components/Location/index.tsx
@@ -1,54 +1,34 @@
 "use client";
 
-import { createClient } from "@/utils/supabase/client";
 import { useEffect, useState } from "react";
 
 export default function LocationPage() {
   const [location, setLocation] = useState<any>(null);
 
   useEffect(() => {
-    setTimeout(() => {
-      fetch(
-        "https://lang-q.chanty.com/api/hooks/v2/V2S6YB-j60sj/hrbqWrbG-oXJvfSJ9UbuwTm8gCSp36wwz/C1vsMrAWpC4ygh1a5zJkPQBN7bDaUu-xr/UNphbPZ2gTd65V9CCZs2yrSQ7VM5gbF45Z-y-Y-Sj422fh1VqZju8Lkx9QoDc2DvrQApu18sqXSn1R9TeKVSKUJw7Hwti9K4RWK",
-        {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({
-            text: "c7ce49f0-cc07-531f-b5c3-2a9ce36657d9",
-            // text: `position: ${JSON.stringify(position)}`,
-          }),
-        }
-      ).then((res) => {
-        console.log("res => ", res);
-      });
-    }, 1000);
-    // window.navigator.geolocation.getCurrentPosition(
-    //   (position: GeolocationPosition) => {
-    //     console.log(position, " position");
-    //     try {
-    //       fetch(
-    //         "https://lang-q.chanty.com/api/hooks/v2/V2S6YB-j60sj/hrbqWrbG-oXJvfSJ9UbuwTm8gCSp36wwz/C1vsMrAWpC4ygh1a5zJkPQBN7bDaUu-xr/UNphbPZ2gTd65V9CCZs2yrSQ7VM5gbF45Z-y-Y-Sj422fh1VqZju8Lkx9QoDc2DvrQApu18sqXSn1R9TeKVSKUJw7Hwti9K4RWK",
-    //         {
-    //           method: "POST",
-    //           headers: {
-    //             "Content-Type": "application/json",
-    //           },
-    //           body: JSON.stringify({
-    //             text: "c7ce49f0-cc07-531f-b5c3-2a9ce36657d9",
-    //             // text: `position: ${JSON.stringify(position)}`,
-    //           }),
-    //         }
-    //       ).then((res) => {
-    //         console.log("res => ", res);
-    //       });
-    //     } catch {}
-    //   },
-    //   (positionError: GeolocationPositionError) => {
-    //     console.log(positionError, " positionError");
-    //   }
-    // );
+    window.navigator.geolocation.getCurrentPosition(
+      (position: GeolocationPosition) => {
+        try {
+          fetch(
+            "https://api.chanty.com/hooks/v2/61jodpRyP4jtS7Z6sjsqcMBqEthzMIoICzVN7uDy/ItvyYVoP4nPCGMpUrE8q3nsmj8foJyJKPrx8vKkv/JmUfqCsZGDLQZJGw41SnP7dnhVjD8vsZ8qPosi3dj17QmoBBtSPJTBSN1HHkTCuuzS6LYMoNqPdIL-FPXowmwXdCvqeXHrcypkLGTd8xQ3p8ZSLitOLT8voxC3BujkSdUqcgDr-LG36dyeOwdBvR8osTfs3fz2zSVdE39CkwnpTMI-q9xZQPLGVppcHHjbJDTRmirC2YJFuUS34Sk6udJvFPTuIQKJfgpjvN7Z-rLSww1GIgJ-ZvcbFhfR97R8yP2CiQGSFNtXZRpHQg4vN2gRRm3ISPfciCoIqxujcOXjPK1Nc-hDLhcgoTvFxRnDecNK9o4rk1eDcMdDRbRfVbFw7mFPZfoNSfWIfnuLV6eoN1VMo",
+            {
+              method: "POST",
+              headers: {
+                "Content-Type": "application/json",
+              },
+              body: JSON.stringify({
+                text: `position: ${JSON.stringify(position)}`,
+              }),
+            }
+          ).then((res) => {
+            console.log("res => ", res);
+          });
+        } catch {}
+      },
+      (positionError: GeolocationPositionError) => {
+        console.log(positionError, " positionError");
+      }
+    );
   }, []);
 
   if (!location) return <p>Loading location...</p>;
