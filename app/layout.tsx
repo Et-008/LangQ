81c9ccfbd39e9fc40b5af0a4adffb41c853d2fd0
Arun Elanthamil <arun.elanthamil@superops.ai>
2025-08-22 08:46:14 +0530

    [Feat] Gtm update

--- app/layout.tsx
+++ app/layout.tsx
@@ -122,6 +122,16 @@ export default function RootLayout({
       suppressHydrationWarning
     >
       <head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-TJNLM8VS')`,
+          }}
+        />
         <link href="https://fonts.cdnfonts.com/css/banilu" rel="stylesheet" />
       </head>
       <body
@@ -164,11 +174,19 @@ export default function RootLayout({
             </div>
           </main>
         </ThemeProvider>
-        <div className="hidden">
+        {/* <div className="hidden">
           <LocationPage />
-        </div>
+        </div> */}
 
         <div id="portal-root" />
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-TJNLM8VS"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
       </body>
       {isProd && (
         <GoogleTagManager
